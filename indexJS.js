70b1ca6bf920b48df3a3fc3ad3778304e36b227d
Aneek Rahman <aneek.rahman8@gmail.com>
2017-10-03 17:54:21 +0600

    Aneek's Profile - V1.0

---
+++ indexJS.js
@@ -0,0 +1,25 @@
+//THE PROFILE SCRIPTINGS HERE
+
+var profileTrigger=0;
+var sideProfileSelect=document.getElementById('sideProfile');
+var profilebtnSelect=document.getElementById('profilebtn');
+var proStyleSelect=document.getElementById('proArrowStyle');
+
+function hideShowSide(){
+    switch(profileTrigger){
+        case 0:
+            sideProfileSelect.style.width='300px';
+            sideProfileSelect.style.padding='20px'; profilebtnSelect.style.backgroundColor="#00bbff";
+            profilebtnSelect.style.color='#fff';
+            proStyleSelect.innerHTML='#sideProfile::before{content: "";border: 10px solid;border-color: transparent transparent transparent #00bbff;font-size: 0;position: fixed;top: 45px;left: 100px;transform: scaleY(5.78) scaleX(2.5);display:block}';
+            profileTrigger=1;
+            break;
+        case 1:
+            sideProfileSelect.style.width='0';
+            sideProfileSelect.style.padding='20px 0';
+            profilebtnSelect.style.backgroundColor="#000";
+            proStyleSelect.innerHTML='#sideProfile::before{display:none}';
+            profileTrigger=0;
+            break;
+    }
+}
