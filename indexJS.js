a83012d8bb2ff72cc0e0efe24c7f6e5d49940187
Aneek Rahman <aneek.rahman8@gmail.com>
2017-10-05 01:39:49 +0600

    Web Profile - V1.7.5

--- indexJS.js
+++ indexJS.js
@@ -8,10 +8,10 @@ var proStyleSelect=document.getElementById('proArrowStyle');
 function hideShowSide(){
     switch(profileTrigger){
         case 0:
-            sideProfileSelect.style.width='300px';
+            sideProfileSelect.style.width='240px';
             sideProfileSelect.style.padding='20px'; profilebtnSelect.style.backgroundColor="#00bbff";
             profilebtnSelect.style.color='#fff';
-           /* proStyleSelect.innerHTML='#sideProfile::before{content: "";border: 10px solid;border-color: transparent transparent transparent #24c0ff;font-size: 0;position: fixed;top: 43px;left: 91px;transform: scaleY(5.3) scaleX(2.5);display:block}'; */
+            proStyleSelect.innerHTML='#sideProfile::before{content: "";border: 10px solid;border-color: transparent transparent transparent #24c0ff;font-size: 0;position: fixed;top: 36px;left: 78px;transform: scaleY(4.6) scaleX(2.5);display:block}';
             profileTrigger=1;
             break;
         case 1:
