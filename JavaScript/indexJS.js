401a33afd1a23a889a34c9e0ddd363a317ebc8d7
Aneek Rahman <aneek.rahman8@gmail.com>
2017-10-09 01:30:14 +0600

    Web Profile - V1.9.1 (Overlay Complete)

--- JavaScript/indexJS.js
+++ JavaScript/indexJS.js
@@ -11,7 +11,7 @@ function hideShowSide(){
             sideProfileSelect.style.width='240px';
             sideProfileSelect.style.padding='20px'; profilebtnSelect.style.backgroundColor="#00bbff";
             profilebtnSelect.style.color='#fff';
-            proStyleSelect.innerHTML='#sideProfile::before{content: "";border: 10px solid;border-color: transparent transparent transparent #24c0ff;font-size: 0;position: fixed;top: 36px;left: 78px;transform: scaleY(4.6) scaleX(2.5);display:block}';
+            proStyleSelect.innerHTML='#sideProfile::before{content: "";border: 10px solid;border-color: transparent transparent transparent #00bbff;font-size: 0;position: fixed;top: 36px;left: 78px;transform: scaleY(4.6) scaleX(2.5);display:block}';
             document.getElementById('borFade').style.display='block';
             profileTrigger=1;
             break;
@@ -25,3 +25,26 @@ function hideShowSide(){
             break;
     }
 }
+// OVERLAY SCRIPTINGS HERE
+var ovClose=document.getElementById('overlayClose');
+var ovOpen=document.getElementById('overlayOpen');
+var ovCont=document.getElementById('overlayCont');
+var ovBack=document.getElementById('overlayBack');
+var body=document.getElementsByTagName('body')[0];
+
+function ovHide(){
+    ovCont.style.display="none";
+    ovBack.style.height="0";
+    ovClose.style.display="none";
+    body.style.overflow="auto";
+}
+function ovShow(){
+    ovCont.style.display="block";
+    ovBack.style.height="100%";
+    ovClose.style.display="block";
+    body.style.overflow="hidden";
+}
+
+ovClose.onclick=ovHide;
+ovOpen.onclick=ovShow;
+ovBack.onclick=ovHide;
